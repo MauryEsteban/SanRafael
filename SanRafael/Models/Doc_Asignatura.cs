c2d875f4cbae9d216e753214e881b5e751bf2726
benjamincj <benja.cruzjarur@gmail.com>
2015-05-25 16:08:18 -0300

    2 commit & push

--- SanRafael/Models/Doc_Asignatura.cs
+++ SanRafael/Models/Doc_Asignatura.cs
@@ -2,6 +2,7 @@ namespace SanRafael.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -12,19 +13,28 @@ namespace SanRafael.Models
         public int id_doc_asignatura { get; set; }
 
         [Column(TypeName = "date")]
+        [DisplayName("Fecha")]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
         public DateTime fecha { get; set; }
 
         [StringLength(500)]
+        [DisplayName("Observacion")]
+        [RegularExpression("[a-zA-Zαινσϊρ0-9 ]{1,500}", ErrorMessage = "Min. 1 y Max. 500 caracteres.")]
         public string observacion { get; set; }
 
         [Required]
         [StringLength(100)]
+        [DisplayName("Ruta")]
         public string path { get; set; }
 
         public bool estado_eliminado { get; set; }
 
+        [DisplayName("Asignatura")]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
         public int id_asignatura_fk { get; set; }
 
+        [DisplayName("Tipo Documento")]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
         public int id_tipo_doc_asignatura_fk { get; set; }
 
         public virtual Asignatura Asignatura { get; set; }
--- SanRafael/Models/Docente.cs
+++ SanRafael/Models/Docente.cs
@@ -2,6 +2,7 @@ namespace SanRafael.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -16,29 +17,48 @@ namespace SanRafael.Models
 
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [DisplayName("Rut")]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [RegularExpression("[0-9]{7,8}[0-9kK]{1}", ErrorMessage = "Min. 8 y Max. 9 caracteres, solo números y letra 'k'.")]
         public int rut_docente { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
         [StringLength(30)]
+        [DisplayName("Nombre")]
+        [RegularExpression("[a-zA-Záéíóúñ]{1,20}", ErrorMessage = "Min. 1 y Max. 20 caracteres, solo letras.")]
         public string nombres { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
         [StringLength(30)]
+        [DisplayName("Apellidos")]
+        [RegularExpression("[a-zA-Záéíóúñ ]{1,30}", ErrorMessage = "Min. 1 y Max. 30 caracteres, solo letras.")]
         public string apellidos { get; set; }
 
+        [DisplayName("Teléfono")]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [RegularExpression("[0-9]{1,10}", ErrorMessage = "Min. 1 y Max. 10 caracteres, solo números.")]
         public int telefono { get; set; }
 
+        [DisplayName("Teléfono2 (opcional)")]
+        [RegularExpression("[0-9]{0,10}", ErrorMessage = "Min. 0 y Max. 10 caracteres, solo números.")]
         public int? telefono2 { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
         [StringLength(50)]
+        [DisplayName("Dirección")]
+        [RegularExpression("[0-9a-zA-Záéíóúñ,.# ]{1,80}", ErrorMessage = "Min. 1 y Max. 80 caracteres.")]
         public string direccion { get; set; }
 
         [StringLength(40)]
+        [DisplayName("Especialidad")]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [RegularExpression("[a-zA-Záéíóúñ ]{1,40}", ErrorMessage = "Min. 1 y Max. 40 caracteres, solo letras.")]
         public string especialidad { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
         [StringLength(20)]
+        [DisplayName("Contraseña")]
+        [RegularExpression("[a-zA-Záéíóúñ ]{1,20}", ErrorMessage = "Min. 1 y Max. 20 caracteres, solo letras.")]
         public string contrasena { get; set; }
 
         public bool estado_eliminado { get; set; }
